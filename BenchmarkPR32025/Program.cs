26fe61bc01e1d029ebd82338675dd64b564be50f
Miha Zupan <mihazupan.zupan1@gmail.com>
2020-02-10 19:26:33 +0100

    Init

---
+++ BenchmarkPR32025/Program.cs
@@ -0,0 +1,109 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace BenchmarkPR32025
+{
+    public class Program
+    {
+        static void Main() => BenchmarkRunner.Run<ToUnsafeOrNotToUnsafe>();
+    }
+
+    public class ToUnsafeOrNotToUnsafe
+    {
+        private readonly string TestUri = string.Concat(Enumerable.Repeat(new string(new char[] { '\ud83f', '\udffe' }), 1000));
+        private static readonly char[] TargetBuffer = new char[12000];
+
+        const int MaxNumberOfBytesEncoded = 4;
+
+        [Benchmark]
+        public unsafe int Unsafe()
+        {
+            ValueStringBuilder vsb = new ValueStringBuilder(TargetBuffer);
+
+            fixed (char* pInput = TestUri)
+            {
+                for (int i = 0; i < TestUri.Length; i += 2)
+                {
+                    UnsafeCore(ref vsb, pInput + i);
+                }
+            }
+
+            return vsb.Length;
+        }
+
+        [Benchmark]
+        public unsafe int Span()
+        {
+            ValueStringBuilder vsb = new ValueStringBuilder(TargetBuffer);
+
+            fixed (char* pInput = TestUri)
+            {
+                for (int i = 0; i < TestUri.Length; i += 2)
+                {
+                    SpanCore(ref vsb, pInput + i);
+                }
+            }
+
+            Debug.Assert(vsb.Length == TargetBuffer.Length);
+
+            return vsb.Length;
+        }
+
+        [Benchmark]
+        public unsafe int SpanSlice()
+        {
+            ValueStringBuilder vsb = new ValueStringBuilder(TargetBuffer);
+
+            fixed (char* pInput = TestUri)
+            {
+                for (int i = 0; i < TestUri.Length; i += 2)
+                {
+                    SpanSliceCore(ref vsb, pInput + i);
+                }
+            }
+
+            return vsb.Length;
+        }
+
+        private static unsafe void UnsafeCore(ref ValueStringBuilder dest, char* pInput)
+        {
+            bool surrogatePair = char.IsSurrogatePair(*pInput, *(pInput + 1));
+            int encodedBytesBuffer;
+            byte* pEncodedBytes = (byte*)&encodedBytesBuffer;
+
+            int encodedBytesCount = Encoding.UTF8.GetBytes(pInput, surrogatePair ? 2 : 1, pEncodedBytes, MaxNumberOfBytesEncoded);
+            Debug.Assert(encodedBytesCount <= MaxNumberOfBytesEncoded, "UTF8 encoder should not exceed specified byteCount");
+
+            for (int count = 0; count < encodedBytesCount; ++count)
+            {
+                UriHelper.EscapeAsciiChar((char)*(pEncodedBytes + count), ref dest);
+            }
+        }
+        private static unsafe void SpanSliceCore(ref ValueStringBuilder dest, char* pInput)
+        {
+            bool surrogatePair = char.IsSurrogatePair(*pInput, *(pInput + 1));
+            Span<byte> encodedBytes = stackalloc byte[MaxNumberOfBytesEncoded];
+            int encodedBytesCount = Encoding.UTF8.GetBytes(new ReadOnlySpan<char>(pInput, surrogatePair ? 2 : 1), encodedBytes);
+            encodedBytes = encodedBytes.Slice(0, encodedBytesCount);
+
+            foreach (byte b in encodedBytes)
+            {
+                UriHelper.EscapeAsciiChar((char)b, ref dest);
+            }
+        }
+        private static unsafe void SpanCore(ref ValueStringBuilder dest, char* pInput)
+        {
+            bool surrogatePair = char.IsSurrogatePair(*pInput, *(pInput + 1));
+            Span<byte> encodedBytes = stackalloc byte[MaxNumberOfBytesEncoded];
+            int encodedBytesCount = Encoding.UTF8.GetBytes(new ReadOnlySpan<char>(pInput, surrogatePair ? 2 : 1), encodedBytes);
+            for (int i = 0; i < encodedBytesCount; i++)
+            {
+                UriHelper.EscapeAsciiChar((char)encodedBytes[i], ref dest);
+            }
+        }
+    }
+}
---
+++ BenchmarkPR32025/UriHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace BenchmarkPR32025
+{
+    class UriHelper
+    {
+        internal static ReadOnlySpan<byte> HexUpperChars => new byte[16]
+        {
+            (byte)'0', (byte)'1', (byte)'2', (byte)'3', (byte)'4', (byte)'5', (byte)'6', (byte)'7',
+            (byte)'8', (byte)'9', (byte)'A', (byte)'B', (byte)'C', (byte)'D', (byte)'E', (byte)'F'
+        };
+
+        internal static void EscapeAsciiChar(char ch, ref ValueStringBuilder to)
+        {
+            to.Append('%');
+            to.Append((char)HexUpperChars[(ch & 0xf0) >> 4]);
+            to.Append((char)HexUpperChars[ch & 0xf]);
+        }
+    }
+}
